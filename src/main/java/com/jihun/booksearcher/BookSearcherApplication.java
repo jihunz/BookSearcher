61278c22beec4be1d7d723baa5eeca87f6d5f45a
jihunz <thespringsong@gmail.com>
2024-03-14 22:46:59 +0900

    first commit

---
+++ src/main/java/com/jihun/booksearcher/BookSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.jihun.booksearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSearcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihun/booksearcher/BookSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihun.booksearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
