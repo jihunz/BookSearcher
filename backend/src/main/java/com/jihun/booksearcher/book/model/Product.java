50a4ae8e8c903cb9bef0f8886878033b1f35d31c
jihunz <thespringsong@gmail.com>
2024-04-02 23:49:32 +0900

    new: bulk index 생성 api 테스트 완료

--- backend/src/main/java/com/jihun/booksearcher/book/model/Product.java
+++ backend/src/main/java/com/jihun/booksearcher/book/model/Product.java
@@ -2,6 +2,7 @@ package com.jihun.booksearcher.book.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
--- backend/src/main/java/com/jihun/booksearcher/book/service/EsServiceImpl.java
+++ backend/src/main/java/com/jihun/booksearcher/book/service/EsServiceImpl.java
@@ -1,15 +1,21 @@
 package com.jihun.booksearcher.book.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.BulkResponse;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.bulk.BulkResponseItem;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import com.jihun.booksearcher.book.model.Product;
 import com.jihun.booksearcher.elasitcSearch.EsConfigV2;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EsServiceImpl {
@@ -18,15 +24,34 @@ public class EsServiceImpl {
     public IndexResponse index() throws IOException {
         ElasticsearchClient esClient = esConfigV2.elasticsearchClient();
 
-        Product product = new Product(1111, "City bike", 9800);
+        List<Product> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Product(i , "product" + i, 1000 * i));
+        }
 
-        IndexResponse res = esClient.index(i -> i
-                .index("products")
-                .id(String.valueOf(product.getId()))
-                .document(product)
-        );
 
-        System.out.println(res);
+        BulkRequest.Builder br = new BulkRequest.Builder();
+
+        for (Product item : list) {
+            br.operations(op -> op
+                    .index(idx -> idx
+                            .index("products")
+                            .id(String.valueOf(item.getId()))
+                            .document(item)
+                    )
+            );
+        }
+
+        BulkResponse result = esClient.bulk(br.build());
+
+        if (result.errors()) {
+            log.error("Bulk had errors");
+            for (BulkResponseItem item: result.items()) {
+                if (item.error() != null) {
+                    log.error(item.error().reason());
+                }
+            }
+        }
 
         return null;
     }
